d3e20e499ce87748a4b9ca39c757b333f80b54d0
Roger <winter30009@yahoo.com.tw>
2022-06-18 09:45:32 +0800

    set up express and mongoose env.

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+
+mongoose.connect(process.env.MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true })
+
+const db = mongoose.connection
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+app.get('/', (req, res) => {
+  res.send('hello world.')
+})
+
+app.listen(3000, () => {
+  console.log('App is now running on http://localhost:3000.')
+})
