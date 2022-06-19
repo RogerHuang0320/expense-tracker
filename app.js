e8995409b374d98d23607a7f3fe847715f5fa41c
Roger <winter30009@yahoo.com.tw>
2022-06-19 08:58:46 +0800

    add index.hbs

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 require('./config/mongoose')
 const express = require('express')
 const exphbs = require('express-handlebars')
+const Record = require('./models/record')
 const app = express()
 
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-  res.render('index')
+  // const userId = req.user._id
+  Record.find({})
+    .lean()
+    .then(Records => res.render('index', { Records }))
+    .catch(error => console.log(error))
 })
 
 app.listen(3000, () => {
