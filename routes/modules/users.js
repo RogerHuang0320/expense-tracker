2b8c6b9ee018568873dc4dafddbac3f0b24c9f7b
Roger <winter30009@yahoo.com.tw>
2022-06-20 15:44:22 +0800

    add some flash and message.hbs

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -13,6 +13,29 @@ router.post('/login', passport.authenticate("local", {
   failureRedirect: '/users/login'
 }))
 
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+router.post('/register', async (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: 'You have to fill up all the blanks.' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: 'Password and confirmPassword are not the same.' })
+  }
+  if (errors.length) {
+    return res.render('register', { errors, name, email, password, confirmPassword })
+  }
+  const salt = await bcrypt.genSalt(10)
+  const hash = await bcrypt.hash(password, salt)
+  await User.create({ name, email, password: hash })
+  req.flash('success_messages', 'You have successfully registered!')
+  res.redirect('/users/login')
+})
+
 router.get('/logout', (req, res, next) => {
   req.logout((err) => {
     res.redirect('/');
