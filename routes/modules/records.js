2d02653ef5395eff902f38995aae3377e35ab47c
Roger <winter30009@yahoo.com.tw>
2022-06-20 14:56:42 +0800

    add edit function

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -3,18 +3,33 @@ const router = express.Router()
 const Record = require('../../models/record')
 const Category = require('../../models/category')
 
-router.get('/new', (req, res) => {
-  const categories = Category.find({}).lean()
-  console.log(categories)
+router.get('/new', async (req, res) => {
+  const categories = await Category.find().lean()
   return res.render('new', { categories })
 })
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   const userId = req.user._id
-  const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
-  return Record.create({ name, userId })     // 存入資料庫
-    .then(() => res.redirect('/')) // 新增完成後導回首頁
-    .catch(error => console.log(error))
+  const { name, date, amount, categoryId } = req.body
+  await Record.create({ name, date, amount, userId, categoryId })     // 存入資料庫
+  res.redirect('/')
+})
+
+router.get('/:id/edit', async (req, res) => {
+  const userId = req.user._id
+  const _id = req.params.id
+  const record = await Record.findOne({ _id, userId }).lean()
+  const categoryId = record.categoryId
+  const categories = await Category.find().lean()
+  const categoryName = categories.filter(category => category._id.equals(categoryId))[0].name
+  record.date = record.date.toJSON().toString().slice(0, 10)
+  return res.render('edit', { record, categories, categoryName })
+})
+
+router.put('/:id/edit', async (req, res) => {
+  const _id = req.params.id
+  await Record.findByIdAndUpdate(_id, req.body)
+  res.redirect('/')
 })
 
 module.exports = router
