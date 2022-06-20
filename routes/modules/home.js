b285c3ed61cf70b53028456523ba14b5ba01e010
Roger <winter30009@yahoo.com.tw>
2022-06-20 14:36:17 +0800

    fix main page and fontawesome version

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,13 +1,36 @@
 const express = require('express')
 const router = express.Router()
 const Record = require('../../models/record')
+const Category = require('../../models/category')
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   const userId = req.user._id
-  Record.find({ userId })
-    .lean()
-    .then(Records => res.render('index', { Records }))
-    .catch(error => console.log(error))
+  const sort = req.query.sort
+  const records = await Record.find({ userId }).lean()
+  const categories = await Category.find().lean()
+  let filterRecord = records
+  let totalAmount = 0
+  //計算totalAmount、拿出存在category的icon、處理時間格式
+  records.forEach(record => {
+    const categoryId = record.categoryId
+    record.icon = categories.filter(category => categoryId.equals(category._id))[0].icon
+    record.date = record.date.toJSON().toString().slice(0, 10)
+    totalAmount += record.amount
+  })
+  //篩選
+  if (sort) {
+    const categoryId = categories.filter(category => category.name === sort)[0]._id
+    totalAmount = 0
+    filterRecord = records.filter(record => record.categoryId.equals(categoryId))
+    filterRecord.forEach(record => {
+      record.icon = categories.filter(category => categoryId.equals(category._id))[0].icon
+      totalAmount += record.amount
+    })
+  }
+  return res.render('index', {
+    records: filterRecord,
+    totalAmount
+  })
 })
 
 module.exports = router
